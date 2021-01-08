89a080e466ad53b6b7c9091c60d89fa00377d7f1
Tully Foote <tfoote@osrfoundation.org>
2021-01-08 00:54:28 -0800

    quick docs update

---
+++ README.md
@@ -0,0 +1,17 @@
+# ghrocker
+
+## Overview
+
+This is an extension for rocker https://github.com/osrf/rocker
+
+It provides bindings to locally test a site expected to be hosted in github pages locally. 
+
+## Usage
+
+    ghrocker PATH_TO_GH_PAGES_REPOSITORY
+
+
+The browse to http://localhost:4000
+Or launch it via:
+
+    sensible-browser http://localhost:4000
--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 import os
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setuptools.setup(
     name='ghrocker',
     version="0.0.1",
@@ -11,12 +15,13 @@ setuptools.setup(
     package_data={'ghrocker': ['templates/*.em']},
     author="Tully Foote",
     author_email="tfoote@osrfoundation.org",
-    description="Plugins for rocker that inject noVNC",
-    long_description="ASDF",
+    description="A rocker extension to locally test github pages.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tfoote/ghrocker",
     license='Apache 2.0',
     install_requires=[
+        'empy',
         'rocker',
     ],
     install_package_data=True,
